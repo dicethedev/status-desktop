6d88229850f91730da6b1dfd0cb17a6de9e1763c
Anton Iakimov <yakimant@gmail.com>
2024-01-19 13:27:08 +0100

    ci: build in nix shell

--- shell.nix
+++ shell.nix
@@ -1,28 +1,39 @@
-{ pkgs ? import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/e7603eba51f2c7820c0a182c6bbb351181caa8e7.zip";
-    sha256 = "sha256:0mwck8jyr74wh1b7g6nac1mxy6a0rkppz8n12andsffybsipz5jw";
-  }) { } }:
+{
+  pkgs ? import ./nix/pkgs.nix
+}:
 
 let
-  qtCustom = with pkgs.qt515;
+  qtCustom = (with pkgs.qt515;
+    # TODO:check the required modules after Qt upgrade
     env "qt-custom-${qtbase.version}" ([
+      qtbase
+      qtdeclarative
+      qtquickcontrols
       qtquickcontrols2
+      qtsvg
+      qtmultimedia
+      qtwebview
+      qttools
+      qtwebchannel
       qtgraphicaleffects
-      qtbase qttools qtdeclarative
-      qtlottie qtmultimedia
-      qtquickcontrols qtquickcontrols2
-      qtsvg qtwebengine qtwebview
-    ]);
+      qtwebengine
+      qtlocation
+#      qtlottie # TODO: was missing in 5.15.2, review after upgrade
+  ]));
+
 in pkgs.mkShell {
   name = "status-desktop-build-shell";
 
+  # TODO:check the required packages after Qt upgrade
   buildInputs = with pkgs; [
-    qt5Full
-    bash curl wget git file unzip jq lsb-release
-    cmake gnumake pkg-config gnugrep qtCustom
-    go_1_19
+    linuxdeployqt
+    libglvnd # TODO: Qt 5.15.2 fix, review after upgrade
+    curl wget git file unzip jq lsb-release
+    cmake_3_19 gnumake pkg-config gnugrep qtCustom
     pcre nss pcsclite extra-cmake-modules
     xorg.libxcb xorg.libX11 libxkbcommon
+    which go_1_20 cacert
+    appimagekit gnupg
   ] ++ (with gst_all_1; [
     gst-libav gstreamer
     gst-plugins-bad  gst-plugins-base
@@ -35,8 +46,66 @@ in pkgs.mkShell {
   LANGUAGE = "en_US.UTF-8";
 
   QTDIR = qtCustom;
+  # TODO: still needed?
+  # https://github.com/NixOS/nixpkgs/pull/109649
+  QT_INSTALL_PLUGINS = "${qtCustom}/${pkgs.qt515.qtbase.qtPluginPrefix}";
 
   shellHook = ''
-    export MAKEFLAGS="-j$NIX_BUILD_CORES"
+    export PATH="${pkgs.lddWrapped}/bin:$PATH"
   '';
+
+  # Used to workaround missing lib links in qt-custom
+  # TODO:check if it's still needed after Qt upgrade
+  LIBRARY_PATH = with pkgs.qt515; pkgs.lib.makeLibraryPath [
+    qtdeclarative
+    qtmultimedia
+    qtquickcontrols
+    qtquickcontrols2
+    qtsvg
+    qtwebchannel
+    qtwebview
+  ];
+
+  # Used for linuxdeployqt
+  # TODO:check if qt modules are still needed here after Qt upgrade
+  LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath (
+  [
+    alsaLib
+    expat
+    fontconfig
+    freetype
+    gcc-unwrapped
+    glib
+    gmp
+    harfbuzz
+    libglvnd
+    libkrb5
+    libpng
+    libpulseaudio
+    libxkbcommon
+    p11-kit
+    zlib
+  ] ++ (with qt515; [
+    qtbase
+    qtdeclarative
+    qtlocation
+    qtmultimedia
+    qtquickcontrols2
+    qtsvg
+    qtwebengine
+  ]) ++ (with xorg; [
+    libICE
+    libSM
+    libX11
+    libXrender
+    libxcb
+    xcbutil
+    xcbutilimage
+    xcbutilkeysyms
+    xcbutilrenderutil
+    xcbutilwm
+  ]) ++ (with gst_all_1; [
+    gst-plugins-base
+    gstreamer
+  ]));
 }
